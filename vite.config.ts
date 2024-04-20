8bb310d31162e3b1053b4d2d186276c8c20c91f2
adam5096 <adamchou5096@gmail.com>
2024-04-20 18:03:01 +0800

    build 3

--- vite.config.ts
+++ vite.config.ts
@@ -23,7 +23,7 @@ export default defineConfig(({ command,mode }) => {
       localEnabled: command === 'serve',//保證開發階段可以使用mock接口
     }),
     ],
-    base: "project"
+    base: "project",
     resolve: {
       alias: {
         "@": path.resolve("./src") // 相对路径别名配置，使用 @ 代替 src
